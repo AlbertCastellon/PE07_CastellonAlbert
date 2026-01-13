e97a568a20bdc1c5945e26f2919e7f511c836df8
Pepe Sufragio <albertkstyon@gmail.com>
2026-01-13 18:32:08 +0100

    validation play

--- PE07.java
+++ PE07.java
@@ -1,10 +1,15 @@
 import java.util.Scanner;
 import java.util.InputMismatchException;
+import java.util.ArrayList;
 
 public class PE07 {
 
     int currentTurn = 0;
     Scanner sc = new Scanner(System.in);
+    char[] xCoordinates = new char[8];
+    char[] yCoordinates = new char[8];
+    ArrayList<String> plays = new ArrayList<String>();
+    
     public static void main(String[] args) {
         PE07 p = new PE07();
         p.principal();
@@ -14,13 +19,19 @@ public class PE07 {
 
         char[][] chessboard = new char[8][8];
         String[] players = new String[2];
-        boolean done = false;
+        for(int i = 0; i < xCoordinates.length; i++){
+            xCoordinates[i] = (char) ('a' + i);
+            yCoordinates[i] = (char) ('1' + i);
+            
+        }
+        
         System.out.println("Introdueix el nom del primer jugador:");
         players[0] = getPlayerName();
         System.out.println("Introdueix el nom del segon jugador:");
         players[1] = getPlayerName(); 
 
         initializeChessboard(chessboard);
+        play(players);
         
 
     }
@@ -62,11 +73,11 @@ public class PE07 {
         return name;
     }
 
-    public void set(boolean done) {
-        boolean finished = false;
+    public void play(String[] players) {
+        boolean done = false;
         String keepPlaying = "";
         do{
-            int winner = play(finished);
+            int winner = game();
 
             System.out.println("Vols seguir jugant?");
             do{
@@ -74,21 +85,67 @@ public class PE07 {
                 if(keepPlaying.equals("no")){
                     done = true;
                 }else if (keepPlaying.equals("si")){
-
+                    if(players[0] != players[winner]){
+                        String loser = players[0]; 
+                        players[0] = players[winner];
+                        players[1] = loser;
+                    }
+                    ;
                 }else {
                     System.out.println("Resposta invàlida");
+                    keepPlaying = "";
                 }
-            }while(keepPlaying.equals(""))
+            }while(keepPlaying.equals(""));
             
         }while(!done);
     }
 
-    public int play(boolean finished) {
+    public int game() {
+        boolean finished = false;
         int winner = 0;
         do{
+            turn();
 
         }while(!finished);
 
         return winner;
+    }
+    public void turn() {
+        int currentPlayer = currentTurn % 2;
+        
+    }
+
+    public String getNextPlay() {
+        String nextPlay = "";
+        do{
+            try {
+                System.out.println("Introdueix la jugada (coordenades d'origen-espai-coordenades de destí ex: e4 f3)");
+                nextPlay = sc.nextLine();
+            }catch(Exception e){
+                System.out.println("Error");
+            }
+            boolean rightFormat = (nextPlay.length() == 5) && (nextPlay.charAt(2) == ' ');
+            boolean inBoundsOrigin = (contains(xCoordinates, nextPlay.charAt(0))) && (contains(yCoordinates, nextPlay.charAt(1)));
+            boolean inBoundsDestination =(contains(xCoordinates, nextPlay.charAt(3))) && (contains(yCoordinates, nextPlay.charAt(4)));
+            if(!rightFormat){
+                System.out.println("Format incorrecte");
+                nextPlay = "";
+            }else if (!inBoundsDestination && !inBoundsOrigin){
+                System.out.println("Coordenades invàlides");
+            }else {
+                plays.add(nextPlay);
+            }
+        }while(!nextPlay.equals(""));
+        return nextPlay;
+    }
+
+    public boolean contains(char[] array, char target) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == target) {
+                return true;
+            }
+        }
+        return false;
+
     }
 }
